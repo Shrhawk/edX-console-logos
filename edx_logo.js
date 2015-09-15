66ac508d823517338558e92f81e37afcc7e2119b
Syed Hassan Raza <shrhawk88@gmail.com>
2015-09-15 03:50:22 -0700

    Browsers Compatibility

--- edx_logo.js
+++ edx_logo.js
@@ -6,12 +6,13 @@
 */
 (function(console) {
   "use strict";
-  var edx_logo_red = 'color: rgba(187,38,105, 0.9);font-size: 53px;font-weight: bolder;font-family: "Microsoft sans serif", "sans-serif", "Arial";line-height: 60%;';
-  var edx_logo_grey = 'color: rgba(140,143,146, 0.7);font-size: 65px;font-weight: bold;margin-left: -13px;font-family: "Courier New", "Courier", monospace;line-height: 60%;';
-  var edx_logo_blue = 'color: rgba(0,165,236, 0.8);font-size: 70px;font-weight: bold;margin-left: -10px;font-family: "Courier New", "Courier", monospace;line-height: 60%;' ;
+  var edx_logo_red = 'color: rgba(187,38,105, 0.9);font-size: 55px;font-weight: bolder;font-family: "Microsoft sans serif", "sans-serif", "Arial";line-height: 60%;';
+  var edx_logo_grey = 'color: rgba(140,143,146, 0.7);font-size: 64px;font-weight: bold;margin-left: -13px;font-family: "FreeMono", "Courier New", "Courier";line-height: 60%;';
+  var edx_logo_blue = 'color: rgba(0,165,236, 0.8);font-size: 69px;font-weight: bold;margin-left: -10px;font-family: "FreeMono", "Courier New", "Courier";line-height: 60%;' ;
   var edx_open_blue = 'color: rgba(0,165,236, 1);font-size: 41px;margin-right: -13px;font-family: "Microsoft sans serif", "sans-serif", "Arial";' ;
   var edx_powered_by_light_grey = 'color: rgb(214,215,216); font-size: 19px;font-weight: lighter;';
   var is_firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+  var ua = navigator.userAgent.toLowerCase();
   var defaults = {
     type: 'auto',
     edx_logo_text: 'Want to contribute to edX-platform ? Visit at https://github.com/edx/edx-platform',
@@ -22,29 +23,59 @@
     open_edx_only: false
   };
 
+  var check_string = function(r) {
+    return r.test(ua);
+  };
+  var os = function(){
+    var isWindows = check_string(/windows|win32/);
+    var isMac = check_string(/macintosh|mac os x/);
+    var isAir = check_string(/adobeair/);
+    var isLinux = check_string(/linux/);
+    return isWindows ? 'Windows' :
+    isMac ? 'Mac' :
+    isAir ? 'Mac' :
+    isLinux ? 'Linux' : 'Other';
+  };
+  var browser = function() {
+    var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+    var isFirefox = typeof InstallTrigger !== 'undefined';// Firefox 1.0+
+    var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    // At least Safari 3+: "[object HTMLElementConstructor]"
+    var isChrome = !!window.chrome && !isOpera;// Chrome 1+
+    var isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6
+    return isOpera ? 'Opera' :
+    isFirefox ? 'Firefox' :
+    isSafari ? 'Safari' :
+    isChrome ? 'Chrome' :
+    isIE ? 'IE' : 'Browser';
+  };
+  function auto_type(case_type){
+    var os_ver = os();
+    var browser_ver = browser();
+    if (browser_ver == 'Firefox' || browser_ver == 'IE' || browser_ver == 'Browser' && case_type == 'image'){
+      console.log(browser_ver + ' does not support images at console.')
+    }else if(browser_ver == 'IE' || browser_ver == 'Browser' && case_type == 'image'){
+      console.log(browser_ver + ' does not support styling at console.')
+    }
+    if (case_type == 'auto'){
+      if (browser_ver == 'IE' || browser_ver == 'Browser'){
+        case_type = 'smalltext';
+      }else {
+        case_type = 'css';
+      }
+    }
+    return case_type;
+  }
   console.edx_logo = function(settings, text) {
     var settings = settings || defaults;
     var text = text || defaults.edx_logo_text;
     var log_text = settings.log_text == true ? true : defaults.log_text;
-    switch (settings.type) {
-      case 'auto':
-      // Firefox does not support url tag
-      if (is_firefox){
-        //css version
-        console.log('%ce%cd%cX', edx_logo_red, edx_logo_grey, edx_logo_blue);
-      }else{
-        //image version
-        console.image(settings.url || defaults.edx_logo_url, settings.scale || 1, log_text, text);
-        log_text = false;
-      }
-      break;
+    switch (auto_type(settings.type)) {
       case 'css':
       console.log('%ce%cd%cX', edx_logo_red, edx_logo_grey, edx_logo_blue);
       break;
       case 'image':
-      if (is_firefox){
-        console.log('Image not supported on Firefox yet.');
-      }
       console.image(settings.url || defaults.edx_logo_url, settings.scale || 1, log_text, text);
       log_text = false;
       break;
@@ -67,21 +98,7 @@
     var text = text || defaults.open_edx_logo_text;
     var log_text = settings.log_text == true ? true : defaults.log_text;
     var open_edx_only = settings.open_edx_only == true ? true: defaults.open_edx_only;
-    switch (settings.type || defaults.type) {
-      case 'auto':
-      if (is_firefox){
-        //css version
-        if(open_edx_only){
-          console.log('%cOPEN %ce%cd%cX', edx_open_blue, edx_logo_red, edx_logo_grey, edx_logo_blue);
-        }else{
-          console.log('%cPOWERED BY \n%cOPEN %ce%cd%cX', edx_powered_by_light_grey, edx_open_blue, edx_logo_red, edx_logo_grey, edx_logo_blue);
-        }
-      }else{
-        //image version
-        console.image(settings.url || defaults.open_edx_logo_url, settings.scale || 0.7, log_text, text);
-        log_text = false;
-      }
-      break;
+    switch (auto_type(settings.type)) {
       case 'css':
       if(open_edx_only){
         console.log('%cOPEN %ce%cd%cX', edx_open_blue, edx_logo_red, edx_logo_grey, edx_logo_blue);
@@ -90,9 +107,6 @@
       }
       break;
       case 'image':
-      if (is_firefox){
-        console.log('Image not supported on Firefox yet.');
-      }
       console.image(settings.url || defaults.open_edx_logo_url, settings.scale || 0.7, log_text, text);
       log_text = false;
       break;
